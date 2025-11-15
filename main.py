1d9b9728fb4a7d4f3400ef51fef6f7341e81fc48
MohitSai-Tatineni <mohitsai.tatineni@gmail.com>
2025-11-15 01:14:36 -0500

    Minor updates

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # set specific origins in prod
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -27,20 +27,17 @@ async def query_travel_agent(query:QueryRequest):
         print(query)
         graph = GraphBuilder(model_provider="groq")
         react_app=graph()
-        #react_app = graph.build_graph()
 
         png_graph = react_app.get_graph().draw_mermaid_png()
         with open("my_graph.png", "wb") as f:
             f.write(png_graph)
 
         print(f"Graph saved as 'my_graph.png' in {os.getcwd()}")
-        # Assuming request is a pydantic object like: {"question": "your text"}
         messages={"messages": [query.question]}
         output = react_app.invoke(messages)
 
-        # If result is dict with messages:
         if isinstance(output, dict) and "messages" in output:
-            final_output = output["messages"][-1].content  # Last AI response
+            final_output = output["messages"][-1].content  
         else:
             final_output = str(output)
         
